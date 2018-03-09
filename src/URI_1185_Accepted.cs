a15ca4c066c754d550622d72ffeab8a4f0f2c2e5
Wesley Heron <wfarias.ti@gmail.com>
2018-03-08 21:40:50 -0300

    .

---
+++ src/URI_1185_Accepted.cs
@@ -0,0 +1,46 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+            char operacao = char.Parse(Console.ReadLine());
+            int contDiagonal = 10;
+
+            double[,] matriz = new double[12, 12];
+            double soma = 0, media = 0;
+
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    matriz[i, j] = double.Parse(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+
+                }
+
+            }
+            for (int k = 0; k <= 10; k++)
+            {
+                for (int p = 0; p <= contDiagonal; p++)
+                {
+                    soma += matriz[k, p];
+                    media = soma / 66;
+                }
+                contDiagonal--;
+            }
+            switch (operacao)
+            {
+                case 'S':
+                    Console.WriteLine("{0:0.0}", soma);
+                    break;
+                case 'M':
+                    Console.WriteLine("{0:0.0}", media);
+                    break;
+                default:
+                    break;
+            }
+
+    }
+
+}
