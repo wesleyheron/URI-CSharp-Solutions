ff4d57cbab8277bc3320685f334aea9e0fbfa864
Wesley Heron <wfarias.ti@gmail.com>
2018-09-10 11:46:24 -0300

    .
    .

---
+++ src/URI_1924_Accepted.cs
@@ -0,0 +1,17 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+            int qtd = int.Parse(Console.ReadLine());
+            string c;
+            for (int i = 0; i < qtd; i++)
+            {
+                c = Console.ReadLine();                
+            }
+            Console.WriteLine("Ciencia da Computacao");
+
+    }
+
+}
