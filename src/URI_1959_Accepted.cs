ec105e681378f59b110b84ba6de8a27fe41dfec7
Wesley Heron <wfarias.ti@gmail.com>
2018-09-10 15:38:00 -0300

    .
    .

---
+++ src/URI_1959_Accepted.cs
@@ -0,0 +1,15 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        string[] v = Console.ReadLine().Split();
+            long n = long.Parse(v[0]);
+            long l = long.Parse(v[1]);
+
+            Console.WriteLine(n*l); 
+
+    }
+
+}
