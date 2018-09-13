c12b01c01fd1a8371dea091d1a1a612abc75e45e
Wesley Heron <wfarias.ti@gmail.com>
2018-09-13 12:10:14 -0300

    .
    .

---
+++ src/URI_2146_Accepted.cs
@@ -0,0 +1,16 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        string v;
+            while ((v = Console.ReadLine()) != null)
+            {
+                int n = int.Parse(v);
+                Console.WriteLine(n-1);
+            }
+
+    }
+
+}
