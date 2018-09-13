d3598d54822a0e41dba50e97ff59c819110baba9
Wesley Heron <wfarias.ti@gmail.com>
2018-09-13 15:22:04 -0300

    .
    .

---
+++ src/URI_2483_Accepted.cs
@@ -0,0 +1,19 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int i = int.Parse(Console.ReadLine());
+            char[] arr = new char[i];
+            string a = "";
+            for (int j = 0; j < i; j++)
+            {
+                a += "a";
+            }
+
+            Console.WriteLine("Feliz nat{0}l!",a);
+
+    }
+
+}
