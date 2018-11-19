f4b8919755ae2efc107a68a500a69976ca27b58f
Wesley Heron <wfarias.ti@gmail.com>
2018-11-19 11:29:04 -0300

    .
    .

---
+++ src/URI_2581_Accepted.cs
@@ -0,0 +1,17 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int qt = int.Parse(Console.ReadLine());
+        string[] questions = new string[qt];
+        for (int i = 0; i < qt; i++)
+        {
+            questions[i] = Console.ReadLine();
+            Console.WriteLine("I am Toorg!");
+        }
+
+    }
+
+}
